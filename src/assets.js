05ae84ae89ae305514192f71918791eb5a5f723e
tschaub <tim.schaub@gmail.com>
2010-02-25 18:18:43 -0500

    Storing and caching assets.

--- src/assets.js
+++ src/assets.js
@@ -1,16 +1,11 @@
 var ASSETS = require("buildkit/assets");
 var FILE = require("file");
 var HANDLER = require("./handler");
-
 var DB = require("google/appengine/ext/db");
+var MEMCACHE = require("google/appengine/api/memcache");
 
-var Article = DB.Model("Article", {
-    title: new db.StringProperty({required: true}),
-    summary: new db.TextProperty(), 
-    content: new db.TextProperty({required: true}),
-    created: new db.DateTimeProperty({autoNowAdd: true}),
-    updated: new db.DateTimeProperty({autoNow: true}),
-    categories: new db.StringListProperty()
+var Asset = DB.Model("Asset", {
+    json: new DB.TextProperty()
 });
 
 // TODO: persist this
@@ -22,10 +17,23 @@ exports.app = HANDLER.App({
     GET: function(env) {
         var path = env.PATH_INFO;
         if (projects[path]) {
+            var str = MEMCACHE.get(path);
+            if (!str) {
+                DB.runInTransaction(function() {
+                    var asset = Asset.getByKeyName(path);
+                    if (!asset) {
+                        asset = new Asset({
+                            keyName: path,
+                            json: JSON.encode(ASSETS.compile(FILE.join("projects", path)))
+                        });
+                        asset.put();
+                    }
+                    str = asset.json;
+                    MEMCACHE.set(path, str);
+                });
+            }
             var request = this.getRequest(env);
             var callback = request.GET("callback");
-            var assets = ASSETS.compile(FILE.join("projects", path));
-            var str = JSON.encode(assets);
             if (callback) {
                 str = callback + "(" + str + ")";
             }
